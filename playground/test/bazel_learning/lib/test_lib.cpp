5a219bf45e845fe0c326fb6d40fd9573422a8b06
Rakesh Sadhu <rakeshsadhu@gmail.com>
2024-07-23 22:45:31 +0200

    learn bazel

---
+++ playground/test/bazel_learning/lib/test_lib.cpp
@@ -0,0 +1,7 @@
+#include "test_lib.h"
+#include <iostream>
+
+void Test::display() const
+{
+    std::cout << "Test::display\n";
+}
---
+++ playground/test/bazel_learning/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "test_lib.h"
+
+int main(void)
+{
+    std::cout << "Hello Main\n";
+    Test t;
+    t.display();
+    return 0;
+}
